9c000a7a30c0d54c2b6018733ca21f4d3c604853
Gavin Draper <gavdraper@users.noreply.github.com>
2023-03-18 07:24:17 +0000

    add Ratings

--- DataPipeline.sql
+++ DataPipeline.sql
@@ -1,25 +1,20 @@
-DROP TABLE IF EXISTS Professions
-DROP TABLE IF EXISTS Cast
-DROP TABLE IF EXISTS CastProfession
-DROP TABLE IF EXISTS CastTitle
-DROP TABLE IF EXISTS TitleType
-
 --Professions
-CREATE TABLE Professions
+DROP TABLE IF EXISTS Profession
+CREATE TABLE Profession
 (
     Id INT NOT NULL IDENTITY,
     Name NVARCHAR(60),
-    CONSTRAINT pk_professions_id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT pk_profession_id PRIMARY KEY CLUSTERED(Id),
 )
 
-INSERT INTO Professions(Name)
+INSERT INTO Profession(Name)
 SELECT DISTINCT
     x.[value]
 FROM
-    master.dbo.[Cast]
+    [raw].[Person]
     CROSS APPLY STRING_SPLIT(primaryProfession, ',') x
 
-UPDATE Professions
+UPDATE Profession
 SET Name = REPLACE(
     STUFF( 
         (SELECT' '+ LTRIM(RTRIM(UPPER(SUBSTRING(value, 1,1))+LOWER(SUBSTRING(value, 2, LEN(value)))))
@@ -29,7 +24,8 @@ SET Name = REPLACE(
    ), ' ', '') 
 
 --Cast
-CREATE TABLE Cast
+DROP TABLE IF EXISTS Person
+CREATE TABLE Person
 (
     Id INT NOT NULL,
     Name NVARCHAR(150),
@@ -38,40 +34,39 @@ CREATE TABLE Cast
     CONSTRAINT pk_cast_id PRIMARY KEY CLUSTERED(Id),
 )
 
-INSERT INTO Cast
+INSERT INTO Person
 SELECT 
     CAST(REPLACE(nconst,'nm','') AS INT) Id,
     CAST(primaryName AS NVARCHAR(150)) [Name],
     CAST(REPLACE(birthYear,'\N','') AS SMALLINT) BirthYear,
     CAST(REPLACE(deathYear,'\N','') AS SMALLINT) DeathYear
 FROM
-    master.dbo.[Cast]
+    [raw].[Person]
 
-UPDATE Cast SET DeathYear = NULL WHERE DeathYear =0
-UPDATE CAST SET BirthYear = NULL WHERE BirthYear = 0
+UPDATE Person SET DeathYear = NULL WHERE DeathYear =0
+UPDATE Person SET BirthYear = NULL WHERE BirthYear = 0
 
-
-SELECT TOP 100 * FROM master.dbo.Cast
 
 --CastProfessions
-CREATE TABLE CastProfession
+DROP TABLE IF EXISTS PersonProfession
+CREATE TABLE PersonProfession
 (
-    CastId INT,
+    PersonId INT,
     ProfessionId INT
 )
-INSERT INTO CastProfession
+INSERT INTO PersonProfession
 SELECT 
-    CastId,
+    PersonId,
    p.Id ProfessionId
 FROM
 (
     SELECT 
-        CAST(REPLACE(nconst,'nm','') AS INT) CastId,
+        CAST(REPLACE(nconst,'nm','') AS INT) PersonId,
         x.VALUE Profession
-    FROM Master.Dbo.Cast
+    FROM raw.Person
         CROSS APPLY STRING_SPLIT(primaryProfession, ',') x
 )x
-INNER JOIN Professions p ON p.Name = REPLACE(
+INNER JOIN Profession p ON p.Name = REPLACE(
     STUFF( 
         (SELECT' '+ LTRIM(RTRIM(UPPER(SUBSTRING(value, 1,1))+LOWER(SUBSTRING(value, 2, LEN(value)))))
          FROM STRING_SPLIT([profession], '_')
@@ -80,32 +75,19 @@ INNER JOIN Professions p ON p.Name = REPLACE(
    ), ' ', '') 
 
 
---CastProfessions
-CREATE TABLE CastTitle
-(
-    CastId INT,
-    TitleId INT
-)
-
-INSERT INTO CastTitle
-SELECT
-    CAST(REPLACE(nconst,'nm','') AS INT) CastId,
-    CAST(REPLACE(REPLACE(x.VALUE,'tt',''),'\N','') AS INT) TitleId
- FROM master.dbo.Cast
-    CROSS APPLY STRING_SPLIT(knownForTitles,',') x
-
 --TitleType
+DROP TABLE IF EXISTS TitleType
 CREATE TABLE TitleType
 (
     Id INT NOT NULL IDENTITY,
     [Name] NVARCHAR(40),
     CONSTRAINT pk_Title_Type PRIMARY KEY CLUSTERED(Id),
 )
-
 INSERT INTO TitleType(Name)
-SELECT DISTINCT TitleType FROM TitleRaw
+SELECT DISTINCT TitleType FROM Raw.Title
 
 --Genre
+DROP TABLE IF EXISTS Genre
 CREATE TABLE Genre
 (
     Id INT NOT NULL IDENTITY,
@@ -115,13 +97,26 @@ CREATE TABLE Genre
 INSERT INTO Genre(Name)
 SELECT DISTINCT
     x.Value
-FROM TitleRaw
+FROM Raw.Title
 CROSS APPLY STRING_SPLIT(Genres,',') x
 WHERE x.[value] <> '\N'
 
 
 --Titles
-SELECT TOP 100 
+DROP TABLE IF EXISTS Title
+CREATE TABLE Title
+(
+    Id INT NOT NULL,
+    TitleTypeId INT,
+    [Name] NVARCHAR(500),
+    OriginalName NVARCHAR(500),
+    IsAdult BIT,
+    StartYear SMALLINT,
+    EndYear SMALLINT,
+    RunTimeMinutes INT
+)
+INSERT INTO Title
+SELECT 
     CAST(REPLACE(tconst,'tt','') AS INT) Id ,
     tt.Id TitleTypeId,
     PrimaryTitle Name,
@@ -129,13 +124,79 @@ SELECT TOP 100
     CAST(IsAdult AS BIT) IsAdult,
     CASE WHEN StartYear = '\N' THEN NULL ELSE CAST(StartYear AS SMALLINT) END StartYear,
     CASE WHEN EndYEar = '\N' THEN NULL ELSE CAST(EndYear AS SMALLINT) END EndYear,
-    CASE WHEN RuntimeMinutes = '\N' THEN NULL ELSE CAST(RuntimeMinutes AS SMALLINT) END RuntimeMinutes,
-    Genres
-FROM TitleRaw    
-LEFT JOIN TitleType tt ON tt.Name = TitleRaw.TitleType
-WHERE PrimaryTitle LIKE '%Mighty Ducks%'
+    CASE WHEN RuntimeMinutes = '\N' THEN NULL ELSE CAST(RuntimeMinutes AS INT) END RuntimeMinutes
+FROM Raw.Title    
+LEFT JOIN TitleType tt ON tt.Name = Raw.Title.TitleType
 
-SELECT DISTINCT TitleType FROM TitleRaw
+--TitleGenre
+DROP TABLE IF EXISTS TitleGenre
+CREATE TABLE TitleGenre
+(
+    TitleId INT,
+    GenreId INT
+)
+INSERT INTO TitleGenre
+SELECT
+    CAST(REPLACE(tconst,'tt','') AS INT) TitleId ,
+    g.Id GenreId
+FROM Raw.Title
+    CROSS APPLY STRING_SPLIT(Genres,',') x
+    LEFT JOIN Genre g ON g.Name = x.Value
+WHERE Genres <> '\N'
 
-sp_rename 
+--CastTitle
+DROP TABLE IF EXISTS Cast
+CREATE TABLE Cast
+(
+    PersonId INT,
+    TitleId INT,
+    Category NVARCHAR(80),
+    Job NVARCHAR(650),
+    Characters NVARCHAR(4000)
+)
+INSERT INTO Cast
+SELECT 
+    CAST(REPLACE(nconst,'nm','') AS INT) PersonId ,
+    CAST(REPLACE(tconst,'tt','') AS INT) TitleId ,
+    CASE WHEN Category = '\N' THEN NULL ELSE category END [Category],
+    CASE WHEN Job = '\N' THEN NULL ELSE job END [Job],
+    CASE WHEN Characters = '\N' THEN NULL ELSE characters END [Characters]
+FROM    
+    Raw.Cast
 
+--Episodes
+DROP TABLE Episode
+SELECT 
+   CAST(REPLACE(tconst,'tt','') AS INT) EpisodeId ,
+   CAST(REPLACE(parenttconst,'tt','') AS INT) TitleId ,
+   CAST(CASE WHEN SeasonNumber = '\N' THEN 0 ELSE SeasonNumber END AS SMALLINT) [SeasonNumber], 
+   CAST(CASE WHEN EpisodeNumber = '\N' THEN 0 ELSE EpisodeNumber END AS INT) [EpisodeNumber]
+INTO Episode   
+FROM Raw.Episode
+
+--Ratings
+DROP TABLE IF EXISTS Rating
+CREATE TABLE Rating
+(
+    TitleId INT,
+    AverageRating INT,
+    TotalVotes INT
+)
+INSERT INTO Rating
+SELECT 
+    CAST(REPLACE(tconst,'tt','') AS INT) TitleId ,
+    AverageRating,
+    NumVotes
+FROM    
+    Raw.Rating
+
+
+
+/*    Keys and Indexes */
+CREATE CLUSTERED INDEX ndx_cast_person_title ON cast(PersonId, TitleId)
+CREATE NONCLUSTERED INDEX ndx_cast_title ON Cast(TitleId)
+CREATE CLUSTERED INDEX ndx_personprofession_person ON PersonProfession(PersonId)
+CREATE CLUSTERED INDEX ndx_title_id ON Title(Id)
+CREATE CLUSTERED INDEX ndx_titlegenre_title ON TitleGenre(TitleId,GenreId)
+CREATE CLUSTERED INDEX ndx_episode_id ON Episode(EpisodeId)
+CREATE  INDEX ndx_episode_title ON Episode(TitleId)
